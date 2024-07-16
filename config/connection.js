1cd6ece5b3cf3565d74e91a82929101e891784b3
girlnotfound <150276019+girlnotfound@users.noreply.github.com>
2024-07-16 09:36:53 -0700

    Trying to deploy to render

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,4 @@
-const Sequelize = require('sequelize');
+/* const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = process.env.DB_URL
@@ -12,5 +12,35 @@ const sequelize = process.env.DB_URL
         dialect: 'postgres',
       }
     );
+
+module.exports = sequelize;
+*/
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+let sequelize;
+
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'postgres',
+    }
+  );
+}
+
+// Test connection (optional)
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection to database has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 module.exports = sequelize;
